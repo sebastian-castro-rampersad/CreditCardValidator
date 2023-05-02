c3f467204e560863ee11cbcfec75c0765a6ea7ac
Sebastián A. Castro Rampersad <sebastian.castro.r@protonmail.com>
2023-05-02 20:54:26 +0200

    Changes in the regular expressions. Change in card number lengths. Remove MIR  and Maestro.

--- Sources/CreditCardValidator/CreditCardValidator.swift
+++ Sources/CreditCardValidator/CreditCardValidator.swift
@@ -1,29 +1,23 @@
 import Foundation
 
 public enum CreditCardType: String {
-    case amex = "^3[47][0-9]{5,}$"
-    case visa = "^4[0-9]{6,}$"
-    case masterCard = "^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$"
-    case maestro = "^(?:5[0678]\\d\\d|6304|6390|67\\d\\d)\\d{8,15}$"
-    case dinersClub = "^3(?:0[0-5]|[68][0-9])[0-9]{4,}$"
-    case jcb = "^(?:2131|1800|35[0-9]{3})[0-9]{3,}$"
-    case discover = "^6(?:011|5[0-9]{2})[0-9]{3,}$"
-    case unionPay = "^62[0-5]\\d{13,16}$"
-    case mir = "^2[0-9]{6,}$"
+    case amex = "^3[47][0-9]*$"
+    case visa = "^4[0-9]*$"
+    case masterCard = "^(?:5[1-5]|222[1-9]|22[3-9]|2[3-6]|27[01][0-9]|2720)[0-9]*$"
+    case dinersClub = "^3(?:[0689]|[68])[0-9]*$"
+    case jcb = "^35(?:2[8-9]|[3-8])[0-9]*$"
+    case discover = "^6(?:011|4[4-9]|5)[0-9]*$"
+    case unionPay = "^62[0-9]*$"
 
     /// Possible C/C number lengths for each C/C type
     /// reference: https://en.wikipedia.org/wiki/Payment_card_number
     var validNumberLength: IndexSet {
         switch self {
-        case .visa:
-            return IndexSet([13,16])
         case .amex:
             return IndexSet(integer: 15)
-        case .maestro:
-            return IndexSet(integersIn: 12...19)
         case .dinersClub:
             return IndexSet(integersIn: 14...19)
-        case .jcb, .discover, .unionPay, .mir:
+        case .visa, .jcb, .discover, .unionPay:
             return IndexSet(integersIn: 16...19)
         default:
             return IndexSet(integer: 16)
@@ -38,12 +32,10 @@ public struct CreditCardValidator {
         .amex,
         .visa,
         .masterCard,
-        .maestro,
         .dinersClub,
         .jcb,
         .discover,
         .unionPay,
-        .mir
     ]
     
     private let string: String
--- Tests/CreditCardValidatorTests/CreditCardValidatorTests.swift
+++ Tests/CreditCardValidatorTests/CreditCardValidatorTests.swift
@@ -14,20 +14,14 @@ final class CreditCardValidatorTests: XCTestCase {
             (.amex, "3787 3449 3671 000"),
             (.visa, "4111 1111 1111 1111"),
             (.visa, "4012 8888 8888 1881"),
-            (.visa, "4222 2222 2222 2"),
             (.masterCard, "5500 0000 0000 0004"),
             (.masterCard, "5555 5555 5555 4444"),
             (.masterCard, "5105 1051 0510 5100"),
             (.masterCard, "2222 4107 4036 0010"),
-            (.maestro, "6771 7980 2100 0008"),
             (.dinersClub, "30569309025904"),
             (.jcb, "3569 9900 1009 5841"),
             (.discover, "6011 0000 0000 0004"),
             (.unionPay, "6250 9470 0000 0014"),
-            (.mir, "2200 0000 0000 0004"),
-            (.mir, "2200 0000 0000 0012"),
-            (.mir, "2200 0000 0000 0020"),
-            (.mir, "2200 0000 0000 0046"),
         ]
         values.forEach { (item) in
             print(item.0)
@@ -45,14 +39,12 @@ final class CreditCardValidatorTests: XCTestCase {
             (.amex, "3715 6536 866"),
             (.visa, "4111 1111 1111 1112"),
             (.visa, "4012 8888 8888 1882"),
-            (.visa, "4222 2222 2222 3"),
             (.visa, "4222 2222 2222"),
             (.masterCard, "5500 0000 0000 0005"),
             (.masterCard, "5555 5555 5555 4445"),
             (.masterCard, "5105 1051 0510 5101"),
             (.masterCard, "2222 4107 4036 0011"),
             (.masterCard, "2222 4107 4036 0011"),
-            (.maestro, "6771 7980 2100 0009"),
             (.dinersClub, "30569309025905"),
             (.dinersClub, "30569309021"),
             (.jcb, "3569 9900 1009 5842"),
@@ -60,11 +52,6 @@ final class CreditCardValidatorTests: XCTestCase {
             (.discover, "6011 0000 0000 0005"),
             (.discover, "6011 0000 001"),
             (.unionPay, "6250 9470 0000 0015"),
-            (.mir, "2200 0000 0000 0005"),
-            (.mir, "2200 0000 0000 0013"),
-            (.mir, "2200 0000 0000 0021"),
-            (.mir, "2200 0000 0000 0047"),
-            (.mir, "2200 0000 0000 004"),
         ]
         values.forEach { (item) in
             print(item.0)
